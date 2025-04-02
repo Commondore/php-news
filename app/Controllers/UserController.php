62c76fd4841a10f010454dcfcd425f30aadbd964
Commondore <alex.som86@gmail.com>
2025-04-02 12:54:36 +0600

    add register page

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -18,4 +18,16 @@ class UserController
     $users = $this->userModel->getAllUsers();
     View::render('users/index.twig', ['users' => $users]);
   }
+
+  public function register()
+  {
+    $name = $_POST['name'] ?? null;
+    $email = $_POST['email'] ?? null;
+    $password = $_POST['password'] ?? null;
+
+    View::render('users/register.twig', ['user' => [
+      'name' => $name,
+      'email' => $email,
+    ]]);
+  }
 }
--- app/Routes/web.php
+++ app/Routes/web.php
@@ -10,5 +10,8 @@ $router->get('/', [HomeController::class, 'index']);
 
 $router->get('/users', [UserController::class, 'index']);
 
+$router->get('/register', [UserController::class, 'register']);
+$router->post('/register', [UserController::class, 'register']);
+
 
 return $router;
