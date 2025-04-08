0becf35c203c285fedb5643726aaaaa0d459b007
Commondore <alex.som86@gmail.com>
2025-04-08 11:12:52 +0600

    add docker build

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -7,18 +7,32 @@ use PDOException;
 
 class Database
 {
-  private static ?PDO $instance = null;
+    private static ?PDO $instance = null;
 
-  public static function connect(): PDO
-  {
-    if(self::$instance === null) {
-      try {
-        self::$instance = new PDO("mysql:host=localhost;dbname=blog", "root", "");
-        self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch (PDOException $e) {
-        die("Подлкючение к базе данных - ошибка: " . $e->getMessage());
-      }
+    public static function connect(): PDO
+    {
+        if (self::$instance === null) {
+            try {
+                // Загружаем .env переменные
+                $rootPath = dirname(__DIR__, 2); // путь до корня проекта
+                $dotenv = \Dotenv\Dotenv::createImmutable($rootPath);
+                $dotenv->load();
+
+                $host = $_ENV['DB_HOST'];
+                $port = $_ENV['DB_PORT'];
+                $dbname = $_ENV['DB_NAME'];
+                $user = $_ENV['DB_USER'];
+                $pass = $_ENV['DB_PASSWORD'];
+
+                $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+
+                self::$instance = new PDO($dsn, $user, $pass);
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Ошибка подключения к базе данных: " . $e->getMessage());
+            }
+        }
+
+        return self::$instance;
     }
-    return self::$instance;
-  }
 }
